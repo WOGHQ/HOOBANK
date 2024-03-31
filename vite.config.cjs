4bb36441ff301b858dfce5a844806886eb266039
WOGHQ <danieljbetancourt4@gmail.com>
2024-03-30 21:02:07 -0700

    Update vite.config.cjs

--- vite.config.cjs
+++ vite.config.cjs
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: 'es2015', // Transpile to ES2015 (which uses CommonJS)
+    target: 'esnext', // Transpile to ES2015 (which uses CommonJS)
   },
 });
