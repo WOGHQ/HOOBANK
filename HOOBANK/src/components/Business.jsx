9ed6f0297963806312e1b9c2b9b4ef4fb65ad20e
WOGHQ <danieljbetancourt4@gmail.com>
2024-03-30 18:07:56 -0700

    Update

---
+++ HOOBANK/src/components/Business.jsx
@@ -0,0 +1,11 @@
+import { features } from '../constants';
+import styles, { layout } from "/Users/danny/Desktop/WOGHQ/HOOBANK/HOOBANK/src/style.js";
+// import Button from './Button';
+
+const Business = () => {
+  return (
+    <div>Business</div>
+  )
+}
+
+export default Business
